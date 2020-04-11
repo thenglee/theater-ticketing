e3ca716c34c12ab1b45253dbb7571f3cc01b86de
Tan Theng Lee <thengleetan@gmail.com>
2020-04-11 15:45:03 +0800

    Optimise data extraction from db in PurchaseAudit and DailyRevenue reports

--- app/reports/purchase_audit.rb
+++ app/reports/purchase_audit.rb
@@ -5,7 +5,7 @@ class PurchaseAudit
   attr_accessor :payment, :user
 
   def self.find_collection
-    Payment.includes(:user).succeeded.all.map do |payment|
+    Payment.includes(:user).succeeded.find_each.lazy.map do |payment|
       PurchaseAudit.new(payment)
     end
   end
@@ -19,7 +19,7 @@ class PurchaseAudit
     column(:reference) { |report| report.payment.reference }
     column(:user_email) { |report| report.user.email }
     column(:user_stripe_id) { |report| report.user.stripe_id }
-    column(:price) { |report| report.payment.to_f }
+    column(:price) { |report| report.payment.price.to_f }
     column(:total_value) { |report| report.payment.full_value.to_f }
   end
 end
