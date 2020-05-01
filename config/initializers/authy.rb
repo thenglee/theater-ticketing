9dbe296c84ef5ccd3ff1f3a36497d3b17bcc8087
Tan Theng Lee <thengleetan@gmail.com>
2020-05-01 18:57:04 +0800

    Update authy key to use env var in production

--- config/initializers/authy.rb
+++ config/initializers/authy.rb
@@ -1,4 +1,4 @@
 require "yaml"
 
-Authy.api_key = Rails.application.secrets.authy_key
+Authy.api_key = Rails.env.production? ? ENV["AUTHY_KEY"] : Rails.application.secrets.authy_key
 Authy.api_uri = "https://api.authy.com"
